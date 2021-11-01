43fe57d30e9e4a3bd4a6d5212e186e79ce767a2b
kondo <t_kondo@alpha-tec.co.jp>
2021-11-01 11:28:27 +0900

    ハードコピー保存ツールを追加

---
+++ PrivateTools/ClipBoadToPng/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ClipBoardToPng
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblout = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(95, 9);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(663, 19);
+            this.txt1.TabIndex = 1;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "出力先フォルダ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "試験ケース";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(95, 38);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(663, 19);
+            this.txt2.TabIndex = 2;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "出力先";
+            // 
+            // lblout
+            // 
+            this.lblout.AutoSize = true;
+            this.lblout.Location = new System.Drawing.Point(95, 75);
+            this.lblout.Name = "lblout";
+            this.lblout.Size = new System.Drawing.Size(35, 12);
+            this.lblout.TabIndex = 6;
+            this.lblout.Text = "label4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(683, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "最小化";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 133);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblout);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblout;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PrivateTools/ClipBoadToPng/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipBoardToPng
+{
+    using System.IO;
+
+    public partial class Form1 : Form
+    {
+        private string FilNameTemplate = "{0}_{1:D3}.png";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.lblout.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var outDir = this.txt1.Text;
+            var subDir = this.txt2.Text;
+
+            var combined = Path.Combine(outDir, subDir);
+
+            if (!Directory.Exists(combined))
+            {
+                Directory.CreateDirectory(combined);
+            }
+
+            // 出力通番を取得
+            var fileNo = 0;
+            foreach (var file in Directory.EnumerateFiles(combined))
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file);
+                var tmpFileNo = 0;
+
+                var split = fileName.Split(new[] { "_" }, StringSplitOptions.RemoveEmptyEntries);
+                if (split.Length != 2)
+                {
+                    continue;
+                }
+
+                var parse = int.TryParse(split[1], out tmpFileNo);
+                if (!parse)
+                {
+                    continue;
+                }
+
+                if (fileNo < tmpFileNo)
+                {
+                    fileNo = tmpFileNo;
+                }
+            }
+
+            fileNo++;
+
+            try
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    var img = Clipboard.GetImage();
+
+                    if (img != null)
+                    {
+                        var bmp = new Bitmap(img);
+
+                        var outputPath = Path.Combine(combined, string.Format(this.FilNameTemplate, subDir, fileNo));
+                        if (File.Exists(outputPath))
+                        {
+                            outputPath = outputPath + "_NEW";
+                        }
+
+                        bmp.Save(outputPath, System.Drawing.Imaging.ImageFormat.Png);
+                        Clipboard.Clear();
+                        img.Dispose();
+                        img = null;
+                        MessageBox.Show("ファイルを保存しました。", "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("エラーが発生しました。詳細:{0}",ex.StackTrace), "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            var outDir = this.txt1.Text;
+            var subDir = this.txt2.Text;
+
+            var combined = Path.Combine(outDir, subDir);
+            var fullPath = Path.GetFullPath(combined);
+
+            this.lblout.Text = fullPath;
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+            var outDir = this.txt1.Text;
+            var subDir = this.txt2.Text;
+
+            var combined = Path.Combine(outDir, subDir);
+            var fullPath = Path.GetFullPath(combined);
+
+            this.lblout.Text = fullPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+    }
+}
